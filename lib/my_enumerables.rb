e2b2da6e85287bc8622722c064e243807b2113a5
Ricardo <ricardomg92@hotmail.com>
2022-03-12 18:41:22 +0100

    project completed

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,59 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    for element in self do
+      yield element, index
+      index += 1
+    end
+    self
+  end
+
+  def my_select
+    values = []
+    my_each { |element| values << element if yield element }
+    values
+  end
+
+  def my_all?
+    values = []
+    my_each { |element| values << element if yield element }
+    eql?(values)
+  end
+
+  def my_any?
+    values = []
+    my_each { |element| values << element if yield element }
+    values.size.positive?
+  end
+
+  def my_none?
+    values = []
+    my_each { |element| values << element if yield element }
+    values.empty?
+  end
+
+  def my_count
+    if block_given?
+      values = []
+      my_each { |element| values << element if yield element }
+      values.size
+    else
+      size
+    end
+  end
+
+  def my_map
+    values = []
+    my_each { |element| values << yield(element) }
+    values
+  end
+
+  def my_inject(initialvalue)
+    value = initialvalue
+    my_each { |element| value = yield(element, value) }
+    value
+  end
 end
 
 # You will first have to define my_each
@@ -9,10 +63,8 @@ end
 class Array
   # Define my_each here
   def my_each
-    index = 0
-    while index < length
-      yield self[index]
-      index += 1
+    for element in self do
+      yield element
     end
     self
   end
